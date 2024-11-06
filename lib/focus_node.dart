dd08b0935774a9f4328b3c1792650227c517895c
Elisey <eliseyozerov@outlook.com>
2024-11-06 12:31:07 +0100

    Misc

--- lib/focus_node.dart
+++ lib/focus_node.dart
@@ -2,7 +2,7 @@ import 'package:better_extensions/build_context.dart';
 import 'package:flutter/widgets.dart';
 
 extension FocusNodeExtension on FocusNode {
-  Rect getRenderRect({RenderObject? ancestor, bool local = false}) {
-    return context?.getRenderRect(ancestor: ancestor, local: local) ?? Rect.zero;
+  Rect getRenderRect({bool local = false}) {
+    return context?.getRenderRect(local: local) ?? Rect.zero;
   }
 }
--- lib/key.dart
+++ lib/key.dart
@@ -2,7 +2,7 @@ import 'package:better_extensions/build_context.dart';
 import 'package:flutter/widgets.dart';
 
 extension GlobalKeyExtension on GlobalKey {
-  Rect getRenderRect({RenderObject? ancestor, bool local = false}) {
-    return currentContext?.getRenderRect(ancestor: ancestor, local: local) ?? Rect.zero;
+  Rect getRenderRect({bool local = false}) {
+    return currentContext?.getRenderRect(local: local) ?? Rect.zero;
   }
 }
